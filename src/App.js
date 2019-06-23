e4d8c74079fd7d9b95f6a2ebc482b74352d80e29
nghuutai <nhtai12435@gmail.com>
2019-06-23 21:36:29 +0700

    Upload file lan dau

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+class App1 extends Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.ten}</h2>
+      </div>
+    );
+  }
+}
+
+function Hello(props){
+  return(
+    <div>
+      <h1>Tôi là {props.name}</h1>
+    </div>
+  )
+}
+const a = "K39A"
+class Info extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Tôi học lớp {a}</h1>
+      </div>
+    );
+  }
+}
+
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        <label className="custom-control custom-checkbox">
+        <input type="checkbox" name id defaultValue="checkedValue" className="custom-control-input" />
+        <span className="custom-control-indicator" />
+        <span className="custom-control-description" />
+      </label>
+      </div>
+    );
+  }
+}
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>xin chao</h1>
+        <App1 ten="huu tai"/>
+        <Hello name="Nguyễn Văn Truyền"/>
+        <Hello name="Nguyễn Hữu Tài"/>
+        <Info/>
+        <Form/>
       </div>
     );
   }
 }
+
+
+
+
 
 export default App;
